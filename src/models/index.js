5b6c37be99c6bbcd2405794699371ec9fee16e57
weinge <wein.geoffrey@gmail.com>
2021-08-11 11:38:04 -0500

    Created relationships

---
+++ src/models/index.js
@@ -0,0 +1,16 @@
+// import models
+const Review = require("./review");
+const Shop = require("./shop");
+const User = require("./user");
+
+// Reviews belongTo Users
+Review.belongsTo(User);
+
+// Shops have many Reviews
+Shop.hasMany(Review);
+
+module.exports = {
+    Review,
+    Shop,
+    User,
+}
