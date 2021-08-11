c21e26a76ac3f88fcc7c1fbc34e6354e2540cb01
weinge <wein.geoffrey@gmail.com>
2021-08-11 12:58:01 -0500

    bcrypt password validation for login api

--- src/routes/api/login.js
+++ src/routes/api/login.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const { User } = require('../../models/index');
 const jwt = require("jsonwebtoken");
 
@@ -7,20 +8,28 @@ const router = express.Router();
 router.post("/login", async (req, res) => {
 
     const email = req.body.email;
-    const hashedPassword = req.body.password;
+    const password = req.body.password;
 
+    // Attempt to pull user from DB
     const userWithEmail = await User.findOne({ where: { email } }).catch((err) => {
         console.log("Error: ", err);
     });
 
-    if(!userWithEmail)
-        return res.json({ message: "Email or password does not match!" });
+    // Check to see if the email is being used
+    if (!userWithEmail) return res.json({ message: "Email or password does not match!" });
 
-    if(userWithEmail.hashedPassword !== hashedPassword)
-        return res.json({ message: "Email or password does not match!" });
-
-    const jwtToken = jwt.sign({ id: userWithEmail.id, email: userWithEmail.email }, process.env.JWT_SEC);
-    res.json({ message: "Welcome back!", token: jwtToken });
+    // Compare plain text password with hashed password stored in DB
+    bcrypt.compare(password, userWithEmail.hashedPassword, function(err, result) {
+        if (result == true) {
+            // Assign JWT upon success
+            const jwtToken = jwt.sign({ id: userWithEmail.id, email: userWithEmail.email }, process.env.JWT_SEC);
+            res.json({ message: "Successfully signed in", token: jwtToken });
+        }
+        else {
+            // Passwords do not match
+            return res.json({ message: "Email or password does not match!" });
+        }
+    })
 });
 
 module.exports = router;
